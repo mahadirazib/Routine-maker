d298d63b12762d96fc7e80f6e2eed0c7a8d9896f
mahadirazib <31607628+mahadirazib@users.noreply.github.com>
2021-11-21 13:25:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,66 @@ document.querySelector("button").addEventListener("click",()=>{
         tableHtml = tableHtml + "</tr>"
     }
     document.querySelector("#Table").innerHTML = tableHtml;
-    document.querySelector("#forButton").innerHTML = "<button id='proceedButton' style='display:block; margin: 20px auto;'>Proceed</button>";
 
+
+
+
+    document.querySelector("#addRowOrColumnButton").innerHTML = "<button id='addRow' style='margin: 10px;'> Add Row </button> <button id='addColumn' style='margin: 10px;'> Add Column </button>";
+    document.querySelector("#addRow").addEventListener("click", function() {
+        let i = 0;
+        let tx = document.getElementsByTagName("textarea");
+        let n = document.querySelector("#Table").innerHTML.split("</tr>")[0].split("</td>").length-1;
+        let rowHtml = "<tr>";
+        for(i=0;i<n;i++){
+                rowHtml = rowHtml + "<td> <textarea class='tableBody' cols='15' rows='1'></textarea> </td>"
+        }
+        rowHtml = rowHtml + "</tr>";
+
+        for(i=0;i<tx.length;i++){
+            document.querySelectorAll("textarea")[i].innerHTML = document.querySelectorAll("textarea")[i].value;
+            document.querySelectorAll("textarea")[i].style.height = "auto";
+            document.querySelectorAll("textarea")[i].style.height = document.querySelectorAll("textarea")[i].scrollHeight;
+        }
+
+        document.querySelector("#Table tbody").innerHTML = document.querySelector("#Table tbody").innerHTML + rowHtml;
+
+    });
+
+
+    document.querySelector("#addColumn").addEventListener("click", function() {
+        let i = 0;
+        let tx = document.getElementsByTagName("textarea");
+        for(i=0;i<tx.length;i++){
+            document.querySelectorAll("textarea")[i].innerHTML = document.querySelectorAll("textarea")[i].value;
+            document.querySelectorAll("textarea")[i].style.height = "auto";
+            document.querySelectorAll("textarea")[i].style.height = document.querySelectorAll("textarea")[i].scrollHeight;
+        }
+
+        let splitedtable = document.querySelector("#Table").innerHTML.split("</tr>");
+        let n = splitedtable.length-1;
+        let finalHtml = "";
+        for(i=0;i<n;i++){
+
+        
+            if(i==0){
+                finalHtml = finalHtml + splitedtable[i] + "<td> <textarea placeholder='Type' class='tableHeader' cols='15' rows='1'></textarea> </td></tr>";;
+            }else{
+                finalHtml = finalHtml + splitedtable[i] + "<td> <textarea class='tableBody' cols='15' rows='1'></textarea> </td></tr>";
+            }
+        }
+        finalHtml = finalHtml + splitedtable[splitedtable.length-1];
+        document.querySelector("#Table tbody").innerHTML = finalHtml;
+
+
+    });
+
+
+
+
+
+
+
+    document.querySelector("#forproceedButton").innerHTML = "<button id='proceedButton' style='display:block; margin: 20px auto;'>Proceed</button>";
 
     document.querySelector("#proceedButton").addEventListener("click", function() {
         let i = 0;
@@ -56,5 +114,3 @@ document.querySelector("button").addEventListener("click",()=>{
 
 
 let tx = document.getElementsByTagName("textarea");
-
-
